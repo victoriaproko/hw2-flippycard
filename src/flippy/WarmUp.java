b472a36f46b6cedf6dd1afbed1ca5054a75cd39a
victoriaproko <victoriaproko333@gmail.com>
2025-09-06 15:19:15 -0700

    countHearts method finished

--- src/flippy/WarmUp.java
+++ src/flippy/WarmUp.java
@@ -11,8 +11,18 @@ public class WarmUp {
 	 * @return the number of cards that are hearts
 	 */
 	public static int countHearts(Card[] cards ){
-		// TODO: fill in the method!
-		return -1;
+		
+		//creates variable to keep track of hearts
+		int heartCounter = 0;
+
+		//cycles through every card in the array of Cards
+		for (Card card : cards) {
+			if (card.getSuit().equals("hearts")) { //cheks if suit is hearts
+				heartCounter += 1;//adds to the counter
+			}
+		}
+
+		return heartCounter; //returns heart card total
 	}
 	
 	/**
@@ -32,10 +42,16 @@ public class WarmUp {
 	// TODO: write the reverseArray method, including method header and JavaDoc comments
 
 	public static void main(String[] args){
-		/*String[] words = {"I", "love", "my", "CS", "classes", "!"};
+		// String[] words = {"I", "love", "my", "CS", "classes", "!"};
 		
-		System.out.println("Before: " + Arrays.toString(words));
-		reverseArray(words);
-		System.out.println("After: " + Arrays.toString(words));*/
+		// System.out.println("Before: " + Arrays.toString(words));
+		// reverseArray(words);
+		// System.out.println("After: " + Arrays.toString(words));
+		Card[] cards = new Card[4];
+        cards[0] = new Card(1, "clubs");
+        cards[1] = new Card(1, "hearts");
+        cards[2] = new Card(3, "hearts");
+        cards[3] = new Card(2, "diamonds");
+        System.out.println(countHearts(cards));
 	}
 }
