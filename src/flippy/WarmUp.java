895fcd535f78c56a60ef8cbab188a24047368c23
victoriaproko <victoriaproko333@gmail.com>
2025-09-06 16:15:51 -0700

    finished addArraysSameLength and reverseArray methods, added comments and test cases

--- src/flippy/WarmUp.java
+++ src/flippy/WarmUp.java
@@ -8,7 +8,7 @@ public class WarmUp {
 	 * Counts how many cards are hearts
 	 * 
 	 * @param cards an array of cards
-	 * @return the number of cards that are hearts
+	 * @return heartCounter: the number of cards that are hearts
 	 */
 	public static int countHearts(Card[] cards ){
 		
@@ -26,32 +26,74 @@ public class WarmUp {
 	}
 	
 	/**
-	 * Add the elements of two arrays of the same length.  The function assumes that 
+	 * Adds the elements of two arrays of the same length.  The function assumes that 
 	 * the arrays are of the same length
 	 * 
-	 * @param array1
-	 * @param array2
-	 * @return a new array that is the element-wise sum of the input arrays 
+	 * @param array1 an array of doubles of a fixed length
+	 * @param array2 an array of doubles of fixed length that is the same length as array1
+	 * @return sumArray: a new array that is the element-wise sum of the input arrays 
 	 */
 	public static double[] addArraysSameLength(double[] array1, double[] array2){
-		// TODO: fill in the method!
-		return null;
+		double[] sumArray = new double[array1.length]; //initializes new array of doubles
+
+		for(int i = 0; i < array1.length; i++) { //cycles through indices of arrays
+			sumArray[i] = array1[i] + array2[i]; //sets index with the new sum value of both arrays
+		}
+		return sumArray; //returns new array
 	}
 	
 		
-	// TODO: write the reverseArray method, including method header and JavaDoc comments
+
+	/**
+	 * Takes an array of Strings as a parameter and reverses the order of the array.
+	 * 
+	 * @param array1 an array of strings
+	 * @return array1: a mutation of the original array of strings provided in parameter
+	 */
+
+	public static String[] reverseArray(String[] array1) {
+		
+		String holder = ""; //creating a variable to hold ont to temporary value being change
+		int i = 0; //first pointer to start at (beginning of array)
+		int x = array1.length-1; //second pointer to start at (end of array)
+
+		while (i < x) { //checking if two pointers have crossed each other
+			holder = array1[i].toString(); //sets holder to the string currently held at this index
+			array1[i] = array1[x]; //swaps content held at indexes
+			array1[x] = holder; // swaps second pointer index with original content of first pointer
+			i++; //increments value
+			x--; //decrements value
+		}
+
+		return array1; //returns modified array
+
+	}
 
 	public static void main(String[] args){
-		// String[] words = {"I", "love", "my", "CS", "classes", "!"};
+		//TESTING reverseArray
+		String[] words = {"I", "love", "my", "CS", "classes", "!"}; //even length
 		
-		// System.out.println("Before: " + Arrays.toString(words));
-		// reverseArray(words);
-		// System.out.println("After: " + Arrays.toString(words));
+		System.out.println("Before: " + Arrays.toString(words));
+		reverseArray(words);
+		System.out.println("After: " + Arrays.toString(words));
+
+		String[] wordsOdd = {"I", "love", "my", "CS", "classes"}; //odd length
+		
+		System.out.println("Before: " + Arrays.toString(wordsOdd));
+		reverseArray(wordsOdd);
+		System.out.println("After: " + Arrays.toString(wordsOdd));
+
+		//TESTING COUNTHEARTS
 		Card[] cards = new Card[4];
         cards[0] = new Card(1, "clubs");
         cards[1] = new Card(1, "hearts");
         cards[2] = new Card(3, "hearts");
         cards[3] = new Card(2, "diamonds");
         System.out.println(countHearts(cards));
+
+		//TESTING addArraysSameLength
+		double[] array1 = {1, 2, 3, 4, 5};
+    	double[] answer = addArraysSameLength(array1, array1);
+      	System.out.println(Arrays.toString(answer));
 	}
 }
